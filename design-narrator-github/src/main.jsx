8b664724b13f633cac39b90b9e6ef521f66c437d
tamaliroyghosh-netizen <tamali.royghosh@gmail.com>
2025-12-10 20:32:59 +0530

    Delete design-narrator-github/src/main.jsx

--- design-narrator-github/src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import AppWrapped from "./App.jsx";
-import "./styles.css";
-
-createRoot(document.getElementById("root")).render(<AppWrapped />);
